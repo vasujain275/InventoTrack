3b39ef076cca3795056162c7220df0dea827be7a
vasujain275 <vasujain275@gmail.com>
2024-06-08 17:27:02 +0530

    feat: added zod and jwt deps

---
+++ api/prisma/migrations/20240608113613_orders/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - Added the required column `totalItems` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `totalPrice` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "totalItems" INTEGER NOT NULL,
+ADD COLUMN     "totalPrice" DOUBLE PRECISION NOT NULL;
