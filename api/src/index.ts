0ac873d1a38e159a567d136288914650945abe0c
vasujain275 <vasujain275@gmail.com>
2024-06-09 13:49:17 +0530

    refactor: fastify redo

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,11 @@
-import fastify from "fastify";
+import Fastify from "fastify";
+import swagger from "@fastify/swagger"
 
-const server = fastify();
+export const server = Fastify();
 
-server.get("/ping", async (request, reply) => {
-  return "pong\n";
-});
 
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+
+
+async function main() {
+
+}
